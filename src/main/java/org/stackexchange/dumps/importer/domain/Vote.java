919c446b73d06bdda7f1e57315bbd5d6b3584dd0
Benedikt Linse <benedikt.linse@gmail.com>
2014-07-10 22:53:50 +0200

    adding support for importing votes

--- src/main/java/org/stackexchange/dumps/importer/domain/Vote.java
+++ src/main/java/org/stackexchange/dumps/importer/domain/Vote.java
@@ -1,11 +1,27 @@
 package org.stackexchange.dumps.importer.domain;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlRootElement;
+import java.util.Date;
+
 
 @Entity
 @XmlRootElement(name = "row")
 public class Vote {
 
+    @Id
+    @XmlAttribute(name = "Id")
+    public int id;
+
+    @XmlAttribute(name = "PostId")
+    public int postId;
+
+    @XmlAttribute(name = "VoteTypeId")
+    public int voteTypeId;
+
+    @XmlAttribute(name = "CreationDate")
+    public Date creationDate;
 
 }
---
+++ src/test/java/org/stackexchange/dumps/importer/unmarshalling/VoteUnmarshallerTest.java
@@ -0,0 +1,27 @@
+package org.stackexchange.dumps.importer.unmarshalling;
+
+import org.junit.Test;
+import org.stackexchange.dumps.importer.GenericUnmarshaller;
+import org.stackexchange.dumps.importer.domain.SeUser;
+import org.stackexchange.dumps.importer.domain.Vote;
+
+import javax.xml.bind.JAXBException;
+
+import static junit.framework.Assert.assertEquals;
+
+public class VoteUnmarshallerTest {
+
+    //   <row Id="87" PostId="33" VoteTypeId="2" CreationDate="2010-07-28T00:00:00.000" />
+    // TODO: test parsing of dates.
+
+    @Test
+    public void test() throws JAXBException {
+        final String line =
+                "<row Id=\"87\" PostId=\"33\" VoteTypeId=\"2\" CreationDate=\"2010-07-28T00:00:00.000\" />";
+        Vote vote = new GenericUnmarshaller<Vote>(Vote.class).unmarshal(line);
+        assertEquals(vote.id, 87);
+        assertEquals(vote.postId, 33);
+        assertEquals(vote.voteTypeId, 2);
+    }
+
+}
