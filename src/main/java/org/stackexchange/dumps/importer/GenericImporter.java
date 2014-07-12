02e182a7eb668a2e1f5607a95938a4515f286745
Benedikt Linse <benedikt.linse@gmail.com>
2014-07-12 23:13:57 +0200

    removing GenericWriter

--- src/main/java/org/stackexchange/dumps/importer/GenericImporter.java
+++ src/main/java/org/stackexchange/dumps/importer/GenericImporter.java
@@ -10,14 +10,19 @@ public class GenericImporter {
     public static <T> void importFile(Session session, final long number, String file, Class<T> t)
             throws FileNotFoundException, JAXBException {
         GenericReader<T> genericReader = new GenericReader<T>(file, t);
-        GenericWriter<T> genericWriter = new GenericWriter<T>(session);
-        genericWriter.open();
         int count = 0;
         while (genericReader.hasNext() && count < number) {
-            genericWriter.write(genericReader.next());
+            session.save(genericReader.next());
             count++;
+            if (count % 20 == 0) {
+                session.flush();
+                session.clear();
+            }
+            if (count % 1000 == 0) {
+                System.err.println("Imported " + count + " entities");
+            }
         }
-        genericWriter.close();
+        session.close();
         genericReader.close();
     }
 
