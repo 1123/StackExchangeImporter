5d704580baebf378cbb839a6843112b9e0e0ab9d
Benedikt Linse <benedikt.linse@gmail.com>
2014-07-24 09:55:03 +0200

    Tests are now runnable with H2. Hence no setup of a postgres database necessary. Testing with postgres is still feasible though.

--- src/test/java/org/stackexchange/dumps/importer/TestConfig.java
+++ src/test/java/org/stackexchange/dumps/importer/TestConfig.java
@@ -6,6 +6,9 @@ import org.hibernate.cfg.ImprovedNamingStrategy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.orm.hibernate4.LocalSessionFactoryBean;
 import org.stackexchange.dumps.importer.domain.*;
 
@@ -16,8 +19,15 @@ import java.util.Properties;
 @ComponentScan("org.stackexchange")
 public class TestConfig {
 
+    @Bean(destroyMethod = "shutdown")
+    public EmbeddedDatabase h2DataSource() {
+        return new EmbeddedDatabaseBuilder().
+                setType(EmbeddedDatabaseType.H2).
+                build();
+    }
+
     @Bean
-    DataSource dataSource() {
+    DataSource postgresDataSource() {
         BasicDataSource result = new BasicDataSource();
         result.setUsername("linse");
         result.setUrl("jdbc:postgresql://localhost:5432/test");
@@ -33,10 +43,14 @@ public class TestConfig {
     @Bean
     LocalSessionFactoryBean sessionFactory() {
         LocalSessionFactoryBean result = new LocalSessionFactoryBean();
-        result.setDataSource(this.dataSource());
+        // you may swith between Postgres, H2 Datasources by commenting out one of the following lines:
+        result.setDataSource(this.h2DataSource());
+        // result.setDataSource(this.postgresDataSource());
         result.setMappingLocations();
         result.setAnnotatedClasses(Post.class, Comment.class, SeUser.class, Vote.class, Badge.class);
         Properties hibernateProperties = new Properties();
+        // TODO: the dialect should be dependent on the selected datasource. PostgresDialect seems to work with H2 currently,
+        // but this may cause trouble in the future.
         hibernateProperties.setProperty("hibernate.dialect", "org.hibernate.dialect.PostgreSQL82Dialect");
         hibernateProperties.setProperty("hibernate.show_sql", "true");
         hibernateProperties.setProperty("hibernate.generate_statistics", "true");
